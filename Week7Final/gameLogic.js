c1a9cade1e79d91b840fa67502bc96ad530a77ec
Josh Peng <pengjos23@gmail.com>
2025-04-27 12:41:35 -0700

    refactor

--- Week7Final/gameLogic.js
+++ Week7Final/gameLogic.js
@@ -1,21 +1,24 @@
 /*
-
-
+    Josh Peng
+    Logic for running the game and creating UI shop buttons.
+    There is no overflow protection but it might take a while to reach that.
 */
 
 // Represents a button in the shop
 class ShopButton {
-    constructor(name, initialCost, growthRate, benefit, image, onClickFunction) {
+    // Constructor initializes a new instance of the ShopButton class
+    constructor(name, initialCost, growthRate, benefit, image, costGrowthFunction, onClickFunction) {
         this.name = name;
         this.cost = initialCost;
         this.rate = growthRate;
         this.benefit = benefit;
         this.bought = 0;
         this.image = image;
+        this.costGrowthFunction = costGrowthFunction;
         this.onClickFunction = onClickFunction;
 
         this.buttonElement = this.makeButton();
-        document.getElementsByClassName("shopContent")[0].appendChild(this.buttonElement);
+        document.getElementsByClassName("shopContent")[0].appendChild(this.buttonElement);      // add button to shopContent
     }
 
     /*
@@ -29,7 +32,7 @@ class ShopButton {
         <div class="shopItemBought">00</div>
     </div>
     */
-    makeButton() {      // this method was imagined, syntax was partially 'vibecoded'
+    makeButton() {      // this method was imagined, syntax for attaching a function was vibecoded lol
         const nButton = document.createElement("div");
         nButton.classList.add("shopButton", "neueFont");
     
@@ -61,23 +64,44 @@ class ShopButton {
         nButton.appendChild(itemBought);
     
         // assign a onclick function that can update the instance variables of this object
-        // nButton.onclick = () => { this.onClickFunction(this); };        // don't know this syntax but it works
-        nButton.onclick = () => { this.handleClick(); };
+        nButton.onclick = () => { this.handleClick(); };        // don't know this syntax but it works
 
         return nButton;
     }
 
+    // Handles when this instance is clicked
     handleClick() {
         if (cheeseCount >= this.cost) {       // do something if can afford
             cheeseCount -= this.cost;
     
             // new_cost = base_cost × (cost_multiplier ^ number_owned)
-            this.cost = Math.ceil(this.cost * this.rate);       // cookie clicker formula I think
+            this.cost = this.costGrowthFunction(this);// Math.ceil(this.cost * this.rate);       // cookie clicker formula I think
             this.bought++;
     
             this.onClickFunction(this.benefit);
         }
     }
+}
+
+// Normal exponential growth function to calculate next cost
+function expGrowth(button) {
+    return Math.ceil(button.cost * button.rate);        // cookie clicker formula I think
+}
+
+// Fast exponential growth function to calculate next cost
+function fastGrowth(button) {
+    return Math.ceil(button.cost * Math.pow(button.rate, button.bought + 1));
+}
+
+// Very specific growth function for next cost
+function piecewiseGrowth(button) {
+    if (button.cost === 100) {
+        return 500;
+    }
+    if (button.cost === 500) {
+        return 10000;
+    }
+    return button.cost * 10;
 }
 
 // Increase passive cheese gains by its formula
@@ -88,27 +112,25 @@ function passiveGain(benefit) {
 
 // Increase active cheese gains by its formula
 function activeGain(benefit) {
-    clickPower += benefit;
+    clickPower *= benefit;
     updateCheeseText();     // update every number in the UI
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-let cheeseCount = 0;
-let passiveRate = 0;
-let clickPower = 1;
-const formatChangePoint = 999999;
+let cheeseCount = 0;                    // cheese in pocket
+let passiveRate = 0;                    // cheese gained every second from farms
+let clickPower = 1;                     // cheese gained from clicking
+const formatChangePoint = 999999;       // point at which numbers are formatted in exp instead
 
-/*                                      name                cost        growth  increase                                            */
-const clickButton =     new ShopButton("Click Power",       60,         4.7,    1,      "./images/cursor.png",  activeGain); //clickButtonClick);
-const ratButton =       new ShopButton("Trained Rat",       15,         1.1,    1,      "./images/rat.png",     passiveGain); //shopButtonClick);
-const manButton =       new ShopButton("Italian Person",    100,        1.12,   3,      "./images/man.png",     passiveGain); //shopButtonClick);
-const cannonButton =    new ShopButton("Cheese Cannon",     1200,       1.14,   8,      "./images/cannon.png",  passiveGain); //shopButtonClick);
-const factoryButton =   new ShopButton("Cheese Factory",    14000,      1.16,   45,     "./images/factory.png", passiveGain); //shopButtonClick);
-const wizardButton =    new ShopButton("Nacho-mancer",      160000,     1.18,   256,    "./images/wizard.png",  passiveGain); //shopButtonClick);
-const raptureButton =   new ShopButton("Cheese Rapture",    1000000,    1.20,   1333,   "./images/rapture.png", passiveGain); //shopButtonClick);
-
-main();
+/*                                      name                cost        growth  gains   pic                     growth func         gains func  */
+const clickButton =     new ShopButton("Click Power",       100,        -1.0,   2,      "./images/cursor.png",  piecewiseGrowth,    activeGain);
+const ratButton =       new ShopButton("Trained Rat",       15,         1.1,    1,      "./images/rat.png",     expGrowth,          passiveGain);
+const manButton =       new ShopButton("Italian Person",    100,        1.12,   3,      "./images/man.png",     expGrowth,          passiveGain);
+const cannonButton =    new ShopButton("Cheese Cannon",     1200,       1.14,   8,      "./images/cannon.png",  expGrowth,          passiveGain);
+const factoryButton =   new ShopButton("Cheese Factory",    14000,      1.16,   45,     "./images/factory.png", expGrowth,          passiveGain);
+const wizardButton =    new ShopButton("Nacho-mancer",      160000,     1.18,   256,    "./images/wizard.png",  expGrowth,          passiveGain);
+const raptureButton =   new ShopButton("Cheese Rapture",    1000000,    1.20,   1333,   "./images/rapture.png", expGrowth,          passiveGain);
 
 function main() {
     updateCheeseText();     // first time init
@@ -116,25 +138,30 @@ function main() {
     setInterval(passiveCheeseIncome, 1000);     // add passive income every 1000ms (1s)
 }
 
+main();
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// Add cheese from passiveRate
 function passiveCheeseIncome() {
     cheeseCount += passiveRate;
     updateCheeseText();
 }
 
+// Add cheese from clickPower
 function onClickCheese() {
     cheeseCount += clickPower;
     updateCheeseText();
 }
 
-// separate function for updating text to account for async and events
+// Separate function for updating all text to account for async and events
 function updateCheeseText() {
     let count = cheeseCount > formatChangePoint ? cheeseCount.toExponential(3) : cheeseCount.toLocaleString();
     let rate = passiveRate > formatChangePoint ? passiveRate.toExponential(3) : passiveRate.toLocaleString();
-    document.getElementById("cheeseCounter").innerText = count + " Cheeses";
-    document.getElementById("cheeseRate").innerText = "per second: " + rate;
+    document.getElementById("cheeseCounter").innerText = count + " Cheeses";        // update cheese counter
+    document.getElementById("cheeseRate").innerText = "per second: " + rate;        // update cheese rate
 
+    // update button texts
     updateShopButton(clickButton);
     updateShopButton(ratButton);
     updateShopButton(manButton);
@@ -144,15 +171,12 @@ function updateCheeseText() {
     updateShopButton(raptureButton);
 }
 
+// Updates the text of a single button in the shop
 function updateShopButton(button) {
-    const cost = button.buttonElement.querySelector(".shopItemCost");       // function was imagined, querySelector was 'vibecoded'
-    cost.innerText = button.cost > formatChangePoint ? button.cost.toExponential(3) : button.cost.toLocaleString();;       // update price
-    if (button.cost > cheeseCount) {        // color changes depending on if can buy
-        cost.style.color = "var(--red)";
-    }
-    else {
-        cost.style.color = "var(--green)";
-    }
-    const bought = button.buttonElement.querySelector(".shopItemBought");       // update bought amount
-    bought.innerText = button.bought;
+    const costElement = button.buttonElement.getElementsByClassName("shopItemCost")[0];        // update cost amount
+    costElement.innerText = button.cost > formatChangePoint ? button.cost.toExponential(3) : button.cost.toLocaleString();;       // update price
+    costElement.style.color = button.cost > cheeseCount ? "var(--red)" : "var(--green)";        // color changes depending on if can afford
+
+    const boughtElement = button.buttonElement.getElementsByClassName("shopItemBought")[0];        // update bought amount
+    boughtElement.innerText = button.bought;
 }
